3a6e8200809eed210600b93a754f11dc4dc2145b
IsmaelKleintjes <45165373+IsmaelKleintjes@users.noreply.github.com>
2020-04-14 15:33:02 +0200

    Uitwerking van de exercise

--- MyLogger.class.php
+++ MyLogger.class.php
@@ -1,19 +1,55 @@
 <?php
 class MyLogger
 {
-    public function log($message, $type) {
+    public function log($message, $type)
+    {
         switch ($type) {
-            case 'INFO' :
-                echo 'INFO: ' . $message . "\n";
+            case 'INFO':
+                $this->info($message);
+                break;
+            case 'ERROR':
+                $this->error($message);
+                break;
+            case 'WARNING':
+                $this->warning($message);
+                break;
+            case 'DEBUG':
+                $this->debug($message);
+                break;
+            default:
+                echo $message;
                 break;
         }
     }
 
-    public function error($message) {
-        echo 'ERROR: ' . $message . "\n";
+    private function logWithTime($message)
+    {
+        echo '[' . date('H:i:s') . '] ' . $message;
+    }
+
+    public function error($message)
+    {
+        $this->logWithTime('ERROR: ' . $message . "\n");
+    }
+
+    public function info($message)
+    {
+        $this->logWithTime('INFO: ' . $message . "\n");
+    }
+
+    public function warning($message)
+    {
+        $this->logWithTime('WARNING: ' . $message . "\n");
+    }
+
+    public function debug($message)
+    {
+        $this->logWithTime('DEBUG: ' . $message . "\n");
     }
 }
+
 
 $logger = new MyLogger();
 $logger->log('Hey! :)', 'INFO');
 $logger->error('This is an error!');
+$logger->warning('warning test');
